fda6f5e831c26609b3d2a081352aebc3512abd1b
William Gardner <william@madebycounter.com>
2024-04-04 17:57:31 -0700

    Refactor

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /app
 COPY src/*.sh .
 RUN chmod +x *.sh
 RUN sh init.sh
-ENTRYPOINT sh start.sh
+RUN rm init.sh
+ENTRYPOINT /opt/flamenco/flamenco-manager
 EXPOSE 8080
--- src/init.sh
+++ src/init.sh
@@ -3,9 +3,8 @@
 cd /tmp
 wget https://flamenco.blender.org/downloads/flamenco-3.4-linux-amd64.tar.gz
 tar -xzf flamenco-3.4-linux-amd64.tar.gz
-mv flamenco-3.4-linux-amd64 /app/flamenco
-chmod +x /app/flamenco/flamenco-manager
-chmod +x /app/flamenco/tools/*
-mkdir -p /app/flamenco/scripts/
+mv flamenco-3.4-linux-amd64 /opt/flamenco
+chmod +x /opt/flamenco/flamenco-manager
+chmod +x /opt/flamenco/tools/*
 rm flamenco-3.4-linux-amd64.tar.gz
 cd -
